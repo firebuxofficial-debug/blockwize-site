4fbe7ade1837f28abdaaf152960a261b063467dd
firebuxofficial-debug <firebuxofficial@gmail.com>
2026-09-18 22:43:54 -0300

    Fix Vercel tRPC function handler

--- api/trpc.ts
+++ api/trpc.ts
@@ -2,7 +2,16 @@ import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { appRouter } from "../server/routers";
 import { createContext } from "../server/_core/context";
 
-export default createExpressMiddleware({
+const trpcMiddleware = createExpressMiddleware({
   router: appRouter,
   createContext,
 });
+
+export default function handler(req: Parameters<typeof trpcMiddleware>[0], res: Parameters<typeof trpcMiddleware>[1]) {
+  return trpcMiddleware(req, res, error => {
+    if (error) {
+      console.error("[tRPC] Request failed", error);
+      if (!res.headersSent) res.status(500).json({ error: "Internal server error" });
+    }
+  });
+}
--- api/trpc/[...trpc].ts
+++
@@ -1,8 +0,0 @@
-import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { appRouter } from "../../server/routers";
-import { createContext } from "../../server/_core/context";
-
-export default createExpressMiddleware({
-  router: appRouter,
-  createContext,
-});
